09b01c9dfa240701957c46cbdd07f10109e801fe
Michael <michael.barry.email@gmail.com>
2021-02-21 12:54:54 -0700

    changes to ssl in db connection

--- server.js
+++ server.js
@@ -13,10 +13,8 @@ const image = require("./controllers/image.js");
 const db = knex({
   client: "pg",
   connection: {
-    host: "postgresql-encircled-76191",
-    user: "postgres",
-    password: "Gerbils1",
-    database: "smart_brain",
+    host: process.env.DATABASE_URL,
+    ssl: true,
   },
 });
 
