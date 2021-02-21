d07406adf7b117e6455f5543c263ea8a9ba362fc
Michael <michael.barry.email@gmail.com>
2021-02-20 22:07:12 -0700

    ready to deploy

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,18 @@
+const Clarifai = require("clarifai");
+
+const app = new Clarifai.App({
+  apiKey: "966aab5427374c7296b15ebdaf6c997c",
+});
+
+const handleApiCall = (req, res) => {
+  app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => res.status(400).json("unable to work with API"));
+};
+
 const handleImage = (req, res, db) => {
   const { id } = req.body;
   db("users")
@@ -12,4 +27,5 @@ const handleImage = (req, res, db) => {
 
 module.exports = {
   handleImage: handleImage,
+  handleApiCall: handleApiCall,
 };
